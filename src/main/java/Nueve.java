5dcd0c63a5ce75f608bd814a61563269c23dbb27
Gabriel Popa <60278374+GabrielOctavianPopa@users.noreply.github.com>
2022-11-05 13:34:41 +0100

    Update Nueve.java
    He quitado el enunciado

--- src/main/java/Nueve.java
+++ src/main/java/Nueve.java
@@ -18,28 +18,3 @@ public class Nueve {
         return initString.equals(nuevoString);
     }
 }
-
-/**
- * Dado un integer x devuelve true si x es un palindromo y false si no lo es
- *
- * ejemplo 1:
- * input x = 121
- * output = true
- * explicacion = 121 se lee 121 si lo lees de derecha a izquiera se lee igual
- *
- * ejemplo 2:
- * x = -121
- * output = false
- *
- * ejemplo 3:
- * x = 10
- * output false
- */
-
-/**
- * extension:
- *
- * resuelve el problema sin convertir x a un string
- */
-
-//for con i-- recorriendo el string

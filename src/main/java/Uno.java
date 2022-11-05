13dbca47f37bbfd7b31110776e2c42cd29185be2
Gabriel Octavian Popa <60278374+TheFalcon515@users.noreply.github.com>
2022-11-05 13:48:47 +0100

    He añadido el problema "Uno"

--- src/main/java/Uno.java
+++ src/main/java/Uno.java
@@ -1,32 +1,42 @@
+import java.util.Arrays;
+
 public class Uno {
 
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        twoSum(nums, target);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+    }
 }
 
 
 /**
- * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * Dado un array NUMS con enteros y un int target, devuelve los indices de los dos numeros que si los sumas son el target
  *
- * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Se puede asumir que cada input tendra exactamente una solucion y que no se puede utilizar el mismo elemento de NUMS dos veces
  *
- * You can return the answer in any order.
+ * Puedes devolver la respuesta en cualquier orden.
  *
- * Example 1:
- *
- * Input: nums = [2,7,11,15], target = 9
- * Output: [0,1]
+ * Ejemplo 1:
+ * Entrada: nums = [2,7,11,15], target = 9
+ * Salida: [0,1]
  * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
- * Example 2:
  *
- * Input: nums = [3,2,4], target = 6
- * Output: [1,2]
- * Example 3:
+ * Ejemplo 2:
+ * Entrada: nums = [3,2,4], target = 6
+ * Salida: [1,2]
  *
- * Input: nums = [3,3], target = 6
- * Output: [0,1]
+ * Ejemplo 3:
+ * Entrada: nums = [3,3], target = 6
+ * Salida: [0,1]
+ *
  * Constraints:
- *
  * 2 <= nums.length <= 104
  * -109 <= nums[i] <= 109
  * -109 <= target <= 109
- * Only one valid answer exists.
+ * solo hay una respuesta validad
  */

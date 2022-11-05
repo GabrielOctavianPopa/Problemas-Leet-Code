4ae9e1c65f5bc1287f255d421d9b5df8a054fab2
Gabriel Octavian Popa <60278374+TheFalcon515@users.noreply.github.com>
2022-11-05 19:48:04 +0100

    He completado el problema "Uno"

--- src/main/java/Uno.java
+++ src/main/java/Uno.java
@@ -1,24 +1,33 @@
-import java.util.Arrays;
-
 public class Uno {
 
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        twoSum(nums, target);
+        int[] nums = {3, 2 ,3};
+        int target = 6;
+        System.out.println(twoSum(nums, target));
     }
 
     public static int[] twoSum(int[] nums, int target) {
-
+        int salida[] = new int[2];
+        //bucle con el primer numero
+        for(int i = 0; i < nums.length; i++) {
+            //bucle con el segundo numero, tiene que acabar este para que se sume 1 al otro
+            //System.out.println("bucle 1: " + nums[i]);
+            for (int j = i + 1; j < nums.length; j++) {
+                //System.out.println("bucle 2: " + nums[j]);
+                if(nums[i] + nums[j] == target){
+                    salida[0] = i;
+                    salida[1] = j;
+                }
+            }
+        }
+        return salida;
     }
 }
 
 
 /**
  * Dado un array NUMS con enteros y un int target, devuelve los indices de los dos numeros que si los sumas son el target
- *
  * Se puede asumir que cada input tendra exactamente una solucion y que no se puede utilizar el mismo elemento de NUMS dos veces
- *
  * Puedes devolver la respuesta en cualquier orden.
  *
  * Ejemplo 1:
